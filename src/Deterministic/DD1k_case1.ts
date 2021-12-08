abc1ca8c1de8414f26abce0521d281fb5ad789a7
Ahmed Magdy <f.ahmadhem@gmail.com>
2021-12-08 11:16:56 +0200

    fix: iterating over possible values for t_i bug is fixed.

--- src/Deterministic/DD1k_case1.ts
+++ src/Deterministic/DD1k_case1.ts
@@ -1,20 +1,22 @@
 import {DD1K} from "./DD1K";
 
-export class DD1k_case1 extends DD1K{
-    Ti : number ;
+export class DD1k_case1 extends DD1K {
+    Ti: number;
 
     constructor(service_time: number, interarrival_time: number, k: number) {
-        super(service_time,interarrival_time,k);
+        super(service_time, interarrival_time, k);
         this.Ti = this.calculate_Ti();
     }
 
     calculate_Ti = (): number => {
-        let answer: number = 0;
-        for (let i = 1; i < 1000; i++) {
-            if ( Math.floor(this.arrival_rate * i + this.epsilon ) - Math.floor(this.service_rate * i - this.service_rate / this.arrival_rate + this.epsilon) == this.k) {
+        let answer: number = 0,
+            i: number = 0;
+        while (true) {
+            if (Math.floor(this.arrival_rate * i + this.epsilon) - Math.floor(this.service_rate * i - this.service_rate / this.arrival_rate + this.epsilon) == this.k) {
                 answer = i;
                 break;
             }
+            i++;
         }
         return answer;
     };
@@ -23,22 +25,22 @@ export class DD1k_case1 extends DD1K{
         let answer: number = 0;
         if (t < 1 / this.arrival_rate) answer = 0;
         else if (t >= 1 / this.arrival_rate && t < this.Ti)
-            answer = Math.floor(this.arrival_rate * t + this.epsilon) - Math.floor(this.service_rate * t - this.service_rate / this.arrival_rate+ this.epsilon);
+            answer = Math.floor(this.arrival_rate * t + this.epsilon) - Math.floor(this.service_rate * t - this.service_rate / this.arrival_rate + this.epsilon);
         else if (t >= this.Ti) {
-            if((1 / this.service_rate) % (1 /this.arrival_rate) == 0)
-                answer =this.k - 1;
-        }else {
+            if ((1 / this.service_rate) % (1 / this.arrival_rate) == 0)
+                answer = this.k - 1;
+        } else {
             //Todo
         }
         return answer;
     };
 
-    waiting_time = (n: number): number =>  {
+    waiting_time = (n: number): number => {
         let answer: number = 0;
-        if(n==0)
+        if (n == 0)
             answer = 0;
-        else if(n < this.arrival_rate * this.Ti)
-            answer = ( (1 / this.service_rate)- (1 / this.arrival_rate)) * (n-1);
+        else if (n < this.arrival_rate * this.Ti)
+            answer = ((1 / this.service_rate) - (1 / this.arrival_rate)) * (n - 1);
         else {
             //Todo
         }
