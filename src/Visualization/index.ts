105af4e252f9f45aa3ad05d98b6531cf94073c58
ahmedEid1 <ahmedeid6422@gmail.com>
2021-12-15 20:06:00 +0200

    fix: adding vis folder

---
+++ src/Visualization/index.ts
@@ -0,0 +1,41 @@
+import Chart from 'chart.js/auto';
+
+
+const ctx = (document.getElementById('myChart') as HTMLCanvasElement).getContext('2d');
+
+const myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            // TODO<refactor>: any is not the best solution > check the correct type
+            backgroundColor: ([
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ] as any),
+            borderColor: ([
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ] as any),
+            borderWidth: 1,
+            stepped: true
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
